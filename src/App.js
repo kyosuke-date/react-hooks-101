ee6e0542b466a2d0f32327397c2deb2c7c9febb2
kyosuke-date <k.date1009@gmail.com>
2020-12-04 21:40:40 +0900

    implement component using ,ultiple states

--- src/App.js
+++ src/App.js
@@ -1,46 +1,28 @@
 import React, { useState } from 'react';
 
-const App = () =>  {
-  const [count, setCount] = useState(0);
+const App = props =>  {
+  const [name, setName] = useState(props.name);
+  const [price, setPrice] = useState(props.price);
 
-  const increment = () => setCount(count + 1);
-  const decrement = () => setCount(count - 1);
-
-  const increment2 = () => setCount(previousCount => previousCount + 1);
-  const decrement2 = () => setCount(previousCount => previousCount - 1);
-  const triple3 = () => setCount(value => {
-    if(value % 3 === 0){
-      return value / 3;
-    } else {
-      return value
-    }
-  });
-
-  const reset = () => setCount(0);
-  const double = () => setCount(count * 2);
-  const triple = () => setCount(count % 3 === 0 ? count / 3 : count);
-  const triple2 = () => setCount(value => value % 3 === 0 ? value / 3 : value);
-
+  const reset = () => {
+    setPrice(props.price);
+    setName(props.name);
+  }
+  
   return (
     <>
-      <div>count: {count}</div>
-      <div>
-        <button onClick={increment}>+1</button>
-        <button onClick={decrement}>-1</button>
-      </div>
-      <div>
-        <button onClick={increment2}>+1</button>
-        <button onClick={decrement2}>-1</button>
-      </div>
-      <div>
-        <button onClick={reset}>reset</button>
-        <button onClick={double}>×2</button>
-        <button onClick={triple}>/3</button>
-        <button onClick={triple2}>/3</button>
-        <button onClick={triple3}>/3</button>
-      </div>
+      <p>現在の{name}は、{price}円です</p>
+      <button onClick={() => setPrice(price + 1)}>+1</button>
+      <button onClick={() => setPrice(price - 1)}>-1</button>
+      <button onClick={reset}>reset</button>
+      <input value={name} onChange={e => setName(e.target.value)} />
     </>
   );
+}
+
+App.defaultProps = {
+  name: '',
+  price: 10000
 }
 
 export default App;
