dbf838fb4778b8cb25e8347a5e14be9849b2260b
kyosuke-date <k.date1009@gmail.com>
2020-12-05 16:52:38 +0900

    implement event creation

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,4 +1,3 @@
-import React, { useReducer } from 'react';
 // action = {
 //   type: 'CREATE_EVENT',
 //   title: '2020 Olynpic in TOLYO',
@@ -19,7 +18,7 @@ const events = (state = [], action) => {
     case 'CREATE_EVENT':
       const event = { title: action.title, body: action.body };
       const length = state.length;
-      const id = length === 0 ? id = 1 : state[length - 1].id + 1;
+      const id = length === 0 ? 1 : state[length - 1].id + 1;
       return [...state, { id, ...event }];
     case 'DELETE_EVENT':
       return state;
