149f3991020a0f5f7e6d9895af822c7d364d774b
kyosuke-date <k.date1009@gmail.com>
2020-12-06 16:00:06 +0900

    delete all operation logs

--- src/components/EventForm.js
+++ src/components/EventForm.js
@@ -1,5 +1,5 @@
 import React, { useState, useContext } from 'react';
-import { CREATE_EVENT, DELETE_ALL_EVENTS, ADD_OPERATION_LOG, DELETE_OPERATION_LOGS } from '../actions/index'
+import { CREATE_EVENT, DELETE_ALL_EVENTS, ADD_OPERATION_LOG, DELETE_OPERATION_LOGS, DELETE_ALL_OPERATION_LOGS } from '../actions/index'
 import AppContext from '../contexts/AppContext';
 import { timeCurrentIso8601 } from '../utils';
 
@@ -41,6 +41,18 @@ const EventForm = () => {
   }
 
   const unCreatable = title === '' || body === '';
+
+  const deleteAllOperationLogs = e => {
+    e.preventDefault();
+    const result = window.confirm('全ての操作ログを削除しても良いですか？')
+
+    if(result){
+      dispatch({
+        type: DELETE_ALL_OPERATION_LOGS
+      })
+    }
+  }
+
   return (
     <>
       <h4>イベント作成フォーム</h4>
@@ -56,6 +68,7 @@ const EventForm = () => {
 
         <button className="btn btn-primary" onClick={addEvent} disabled={unCreatable}>イベントを作成する</button>
         <button className="btn btn-danger" onClick={deleteALLEvents} disabled={state.events.length === 0}>全てのイベントを削除する</button>
+        <button className="btn btn-danger" onClick={deleteAllOperationLogs} disabled={state.operationLogs.length === 0}>全ての操作ログを削除する</button>
       </form>
     </>
   )
